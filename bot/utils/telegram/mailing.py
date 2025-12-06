f28a9f9449e09e45410b278dcfd41624d03e472f
Caportabow <caportabow@gmail.com>
2025-12-06 14:25:26 +0100

    fix: rename _sender to _send and update database connection handling in mailing.py

--- bot/utils/telegram/mailing.py
+++ bot/utils/telegram/mailing.py
@@ -2,6 +2,7 @@ import asyncio
 from aiogram import Bot
 from aiogram.exceptions import TelegramNotFound, TelegramForbiddenError, TelegramNetworkError
 
+from db import init_db, close_db
 from db.chats import get_all_chat_ids, forget_chat
 from config import TELEGRAM_TOKEN, MAINTENANCE_PICTURE_ID, UPDATE_PICTURE_ID
 
@@ -9,7 +10,7 @@ bot = Bot(token=TELEGRAM_TOKEN)
 
 
 # Отправка фото по file_id
-async def _sender(chat_id: int, file_id: str, caption: str | None = None):
+async def _send(chat_id: int, file_id: str, caption: str | None = None):
     try:
         await bot.send_photo(chat_id=chat_id, photo=file_id, caption=caption, parse_mode="HTML")
 
@@ -22,7 +23,7 @@ async def _sender(chat_id: int, file_id: str, caption: str | None = None):
     except TelegramNetworkError as e:
         # Проблемы с сетью/таймаут
         await asyncio.sleep(0.6)
-        await _sender(chat_id, file_id, caption)
+        await _send(chat_id, file_id, caption)
 
     except Exception as e:
         # Неверный file_id, неподдерживаемый формат,
@@ -43,11 +44,15 @@ async def _preparer(file_id: str, caption: str | None = None, limit: int = 5):
     await asyncio.gather(*tasks)
 
 
-async def send(active: bool, text: str = "⚙️ Ваше сообщение.\n\n"):
+async def sender(active: bool, text: str = "⚙️ Ваше сообщение.\n\n"):
     picture = UPDATE_PICTURE_ID if active else MAINTENANCE_PICTURE_ID
     # Передаем file_id вместо пути к файлу
-    await _preparer(picture, caption=text)
+    try:
+        await init_db()
+        await _preparer(picture, caption=text)
+    finally:
+        await close_db()
 
 
 if __name__ == "__main__":
-    asyncio.run(send(False))
+    asyncio.run(sender(False))
