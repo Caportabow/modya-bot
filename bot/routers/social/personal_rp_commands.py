8056a07e62926677f63fea507e7a20747da8a383
Caportabow <caportabow@gmail.com>
2026-01-24 19:54:30 +0100

    feat: rework of mrp user interface

--- bot/routers/social/personal_rp_commands.py
+++ bot/routers/social/personal_rp_commands.py
@@ -21,12 +21,12 @@ async def show_rp_commands_handler(msg: Message):
     """Команда: мрп список"""
     commands = await get_user_rp_commands(int(msg.chat.id), int(msg.from_user.id))
     if not commands:
-        await msg.reply("📜 Кастомные RP-команды отсутствуют.")
+        await msg.reply("🖋 Кастомные RP-команды отсутствуют.")
         return
     
-    lines = ["📜 Кастомные RP-команды:\n"]
-    lines.append("<blockquote expandable>")
-    
+    ans = "🖋 Кастомные RP-команды:\n\n"
+    ans += "<blockquote expandable>"
+
     for command, template in commands.items():
         # Разделяем эмодзи и остальной текст, если в шаблоне есть "•"
         if "•" in template:
@@ -38,11 +38,11 @@ async def show_rp_commands_handler(msg: Message):
             emoji = ""
             action = template.strip()
         
-        lines.append(f"{command} {emoji} —> {action}")
-    lines.append("</blockquote>\n")
+        ans += f"{command} {emoji} —> {action}\n"
+    ans += "</blockquote>\n\n"
+    ans += f"Всего: {len(commands)}"
 
-    lines.append(f"Всего: {len(commands)}")
-    await msg.reply("\n".join(lines), parse_mode="HTML")
+    await msg.reply(ans, parse_mode="HTML")
 
 @router.message(
     F.text.regexp(r"^мрп экспорт(?:\s|$)", flags=re.IGNORECASE)
@@ -66,7 +66,6 @@ async def export_rp_commands_handler(msg: Message):
         if source_chat_id == current_chat_id:
             return await msg.reply("❌ Нельзя экспортировать команды в этот же чат.")
 
-        # Вызываем "умную" функцию экспорта
         added_cmds = await export_rp_commands(
             source_chat_id, 
             user_id, 
@@ -76,20 +75,19 @@ async def export_rp_commands_handler(msg: Message):
 
         if added_cmds:
             # Склеиваем первые 5 команд для примера в сообщении
-            cmd_list = ", ".join(added_cmds[:5])
+            cmd_list = "\n".join(added_cmds[:5])
             more = f" и еще {len(added_cmds) - 5}..." if len(added_cmds) > 5 else ""
             
             await msg.reply(
-                f"✅ <b>Экспорт завершен!</b>\n"
-                f"Добавлено новых команд: <b>{len(added_cmds)}</b>\n"
-                f"Список: <code>{cmd_list}{more}</code>",
+                f"✅ Добавлено {len(added_cmds)} команды:\n\n"
+                f"<code>{cmd_list}</code>{more}",
                 parse_mode="HTML"
             )
         else:
             await msg.reply(
-                "❌ <b>Экспорт не удался</b>\n\n"
+                "❌ Ошибка экспорта\n\n"
                 "Возможные причины:\n"
-                "1. Команды из того чата уже добавлены.\n"
+                "1. Команды уже добавлены.\n"
                 "2. Будет превышен лимит команд в этом чате.\n"
                 "3. У вас нет команд в указанном чате.",
                 parse_mode="HTML"
@@ -98,7 +96,7 @@ async def export_rp_commands_handler(msg: Message):
     else:
         # Если ввели просто "мрп экспорт" — даем инструкцию
         await msg.reply(
-            f"Чтобы перенести команды из этого чата в другой, введите там:\n"
+            f"🖋 Чтобы перенести команды из этого чата в другой, введите там:\n"
             f"<code>мрп экспорт {msg.chat.id}</code>", 
             parse_mode="HTML"
         )
@@ -142,7 +140,7 @@ async def set_rp_command(msg: Message):
         return
 
     await upsert_command(int(msg.chat.id), int(msg.from_user.id), command, emoji_text, action)
-    await msg.reply(f"🎭 Команда добавлена успешно")
+    await msg.reply(f"🖋 Команда добавлена успешно")
 
 @router.message(
     (F.text.regexp(r"^-мрп(?:\s|$)", flags=re.IGNORECASE))
@@ -167,6 +165,6 @@ async def unset_rp_command(msg: Message):
     result = await delete_rp_command(int(msg.chat.id), int(msg.from_user.id), command)
     
     if result:
-        await msg.reply(f"🎭 Команда удалена успешно")
+        await msg.reply(f"🖋 Команда удалена успешно")
     else:
         await msg.reply(f"❌ Команда не найдена")
